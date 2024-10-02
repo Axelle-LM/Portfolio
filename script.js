b5bcd4c15852e00186e9b42aecd28fb6aaae956f
Axelle-LM <98803198+Axelle-LM@users.noreply.github.com>
2024-10-02 11:14:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// Récupérer le nom du lien cliqué depuis le localStorage
+const clickedLinkName = localStorage.getItem('clickedLinkName');
+
+// Lire le fichier data.json avec fetch
+fetch('data.json')
+    .then(response => response.json()) // Convertir la réponse en JSON
+    .then(jsonData => {
+        // Vérifier si clickedLinkName existe et est dans jsonData
+        if (clickedLinkName && jsonData[clickedLinkName]) {
+            for (i of ["name", "contexte", "presentation", "role", "technologie"]) {
+                if (jsonData[clickedLinkName][i] != null) {
+                    document.getElementsByClassName(i)[0].innerHTML = jsonData[clickedLinkName][i];
+                }
+                else {
+                    if (i == "role") {
+                        document.getElementsByClassName("role-div")[0].remove()
+                    } else {
+                        document.getElementsByClassName(i)[0].remove()
+                    }
+                }
+            }
+            document.querySelector(".heading").style.backgroundImage = window.getComputedStyle(document.querySelector(".heading")).backgroundImage.replace(/url\(["']?[^"']*["']?\)/, `url("${jsonData[clickedLinkName]["img_background"]}")`);
+        }
+        /*
+            document.getElementsByClassName('titre-projet')[0].innerHTML = jsonData[clickedLinkName]["name"];
+            document.getElementsByClassName('contexte')[0].innerHTML = jsonData[clickedLinkName]["contexte"];
+            document.getElementsByClassName('presentation')[0].innerHTML = jsonData[clickedLinkName]["presentation"];
+            if (jsonData[clickedLinkName]["role"] != null) { document.getElementsByClassName('role')[0].innerHTML = jsonData[clickedLinkName]["role"] } else { document.getElementsByClassName('rolediv')[0].remove() };
+            document.getElementsByClassName('technologie')[0].innerHTML = jsonData[clickedLinkName]["technologie"];
+        }
+        */
+    })
+    .catch(error => {
+        console.error('Erreur lors du chargement du fichier JSON :', error);
+    });
